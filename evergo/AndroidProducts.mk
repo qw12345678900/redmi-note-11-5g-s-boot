972f775be8a1946a50eb1c7629e5fdda0e98f4c8
qw12345678900 <qw12345678900@users.noreply.github.com>
2026-06-20 00:33:11 +0000

    Add evergo device tree

---
+++ evergo/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_evergo.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_evergo-user \
+    omni_evergo-userdebug \
+    omni_evergo-eng
