06064332976aab2650c3c676cba85685cc098667
qw12345678900 <127608408+qw12345678900@users.noreply.github.com>
2026-06-22 19:03:55 +0800

    Use TWRP common config for twrp-12.1 build

--- omni_evergo.mk
+++ omni_evergo.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit TWRP common config.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from evergo device
 $(call inherit-product, device/xiaomi/evergo/device.mk)
